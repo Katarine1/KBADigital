1e6ee039764359ce83f4858e78a7e220d8987039
Katarine Albuquerque <kba.2879@gmail.com>
2021-12-28 16:47:09 -0300

    Url LinkedIn editada Footer

--- src/componentes/Links/LinkLinkedinFooter.js
+++ src/componentes/Links/LinkLinkedinFooter.js
@@ -4,7 +4,7 @@ import ImagemLinkedin from './../../imagens/site/linkedin.png';
 
 const LinkLinkedinFooter = () => {
     return (
-        <a id="imagem-linkedin" href="https://www.linkedin.com/in/katarine-albuquerque/" target="_blank">
+        <a id="imagem-linkedin" href="https://www.linkedin.com/in/nome-perfil" target="_blank">
             <img id="footer-linkedin" src={ImagemLinkedin} alt="imagem"/>
         </a>
     );
