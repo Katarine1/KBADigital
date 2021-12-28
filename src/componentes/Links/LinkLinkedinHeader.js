91d69ec7f64d2a1036aa9a609d2fbcd9820e016d
Katarine Albuquerque <kba.2879@gmail.com>
2021-12-28 16:48:11 -0300

    Url LinkedIn editada Header

--- src/componentes/Links/LinkLinkedinHeader.js
+++ src/componentes/Links/LinkLinkedinHeader.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LinkLinkedinHeader = () => {
     return(
-        <a href="https://www.linkedin.com/in/katarine-albuquerque/" target="_blank">Linkedin</a>
+        <a href="https://www.linkedin.com/in/nome-perfil" target="_blank">Linkedin</a>
     );
 }
 
