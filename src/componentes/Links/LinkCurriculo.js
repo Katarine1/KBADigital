a1c10e8646ced17070870de7df73189040d95a9f
Katarine Albuquerque <kba.2879@gmail.com>
2021-12-28 16:42:00 -0300

    Url editada

--- src/componentes/Links/LinkCurriculo.js
+++ src/componentes/Links/LinkCurriculo.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LinkCurriculo = () => {
     return(
-        <a id="curriculo" href="https://drive.google.com/file/d/1ejF4UejawTv_2uhfKfzldFNdd-pLDSzZ/view?usp=sharing" target="_blank">
+        <a id="curriculo" href="" target="_blank">
             Veja meu Currículo
         </a>
     );
